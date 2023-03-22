b256cbe1a2e9a150486c5b8fd7adec4b60215f34
nishy22 <nishadinimeshika22@gmail.com>
2023-03-22 21:15:00 +0000

    home

--- app/src/main/java/com/example/you/MainActivity.java
+++ app/src/main/java/com/example/you/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.you;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.you.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new Home());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new Home());
+                    break;
+
+                case R.id.category:
+                    replaceFragment(new Category());
+                    break;
+
+                case R.id.wishlist:
+                    replaceFragment(new Wishlist());
+                    break;
+
+                case R.id.account:
+                    replaceFragment(new Account());
+                    break;
+            }
+
+            return true;
+
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
