bd3f3f8ac32a44e2ae189ffa14fbe583cf95b0fd
nishy22 <nishadinimeshika22@gmail.com>
2023-03-23 02:40:53 +0000

    home

--- app/src/main/java/com/example/you/MainActivity.java
+++ app/src/main/java/com/example/you/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.you;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.you.databinding.ActivityMainBinding;
 
@@ -46,7 +51,28 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.tool_bar_menu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.cart:
+                replaceFragment(new Cart());
+                break;
+       }
+
+        return  super.onOptionsItemSelected(item);
+    }
+
+
 
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
